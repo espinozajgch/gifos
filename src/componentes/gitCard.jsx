f639baaa8e45321920c6eedbac0865a9827f83ba
Jose Espinoza <espinozajgx@gmail.com>
2021-11-01 16:08:03 -0300

    ajustes finales

--- src/componentes/gitCard.jsx
+++ src/componentes/gitCard.jsx
@@ -2,8 +2,10 @@
 function GitCard(props){
     return(
         <div className="git-card">
+            <a href={props.image.images.original.url} target="_blank">
             <img src={props.image.images.original.url}  className="card-img" alt={props.image.title} />
             {/*<h5>{props.image.title}</h5>*/}
+            </a>
         </div>
     );
 }
